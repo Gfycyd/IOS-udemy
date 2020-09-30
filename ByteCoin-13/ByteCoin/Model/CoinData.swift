e9a296781668e70fc3a54d0ae2f5e1aa20e6cd01
Gfycyd <31628809+Gfycyd@users.noreply.github.com>
2020-09-30 16:15:57 +0300

    API challenge
    UIPicker + refresh the delegate + work with API + ParseJson + Extensions

---
+++ ByteCoin-13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Ekaterina Uzbekova on 30.09.2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData:Decodable {
+    let rate: Double
+}
